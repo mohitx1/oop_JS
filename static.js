06c8ab37a7d15351b4ae005d187506cee31d8b74
mohitx1 <mohitrockx3@gmail.com>
2023-09-09 15:54:49 +0530

    added id

--- static.js
+++ static.js
@@ -1,22 +1,60 @@
-//utility functions
+//increment
+
+let id=1
 class user{
-    constructor(name,age){
+    constructor(name,age,income){
         this.name=name;
         this.age=age;
+        this.income=income;
+        this.id = id++;
     
     }
 
     static compareAge(user1,user2){
         return user1.age-user2.age
     }
+
+    static compareIncome(user1,user2){
+        return user1.income-user2.income
+    }
 }
 
-const user1= new user('mohit',27)
-const user2= new user('kohit',37)
-const user3= new user('lohit',47)
-const user4= new user('tohit',17)
+const user1= new user('mohit',27,47000)
+const user2= new user('kohit',37,4144)
+const user3= new user('lohit',47,5555)
+const user4= new user('tohit',17,5532)
 
 
-const users=[user1,user2,user3,user4]
-users.sort(user.compareAge)
-console.log(users)
+
+console.log(user1,user2,user3,user4)
+
+// //<---->
+
+// //utility functions
+// class user{
+//     constructor(name,age,income){
+//         this.name=name;
+//         this.age=age;
+//         this.income=income;
+    
+//     }
+
+//     static compareAge(user1,user2){
+//         return user1.age-user2.age
+//     }
+
+//     static compareIncome(user1,user2){
+//         return user1.income-user2.income
+//     }
+// }
+
+// const user1= new user('mohit',27,47000)
+// const user2= new user('kohit',37,4144)
+// const user3= new user('lohit',47,5555)
+// const user4= new user('tohit',17,5532)
+
+
+// const users=[user1,user2,user3,user4]
+// // users.sort(user.compareAge)
+// users.sort(user.compareIncome)
+// console.log(users)
